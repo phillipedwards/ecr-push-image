edbd28fd9b317a28f0f5892e683b4d838bf2283b
Phillip Edwards <phillip@pulumi.com>
2022-08-24 21:40:42 -0700

    added materializeinc docker provider, remoteImage, and made reusable function

--- index.ts
+++ index.ts
@@ -1,9 +1,57 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as materialDocker from "@materializeinc/pulumi-docker-buildkit";
 import * as docker from "@pulumi/docker";
-import * as command from "@pulumi/command";
+import { local } from "@pulumi/command";
 
-const getEcrCredentials = (repo: aws.ecr.Repository): docker.ImageRegistry => {
+export = async () => {
+    const config = new pulumi.Config();
+    const imageTag = config.require("imageTag");
+
+    const service = await constructRepoAndBuildImage(imageTag, "service", "pulumi/service");
+    const ui = await constructRepoAndBuildImage(imageTag, "ui", "pulumi/console");
+    const migrations = await constructRepoAndBuildImage(imageTag, "migrations", "pulumi/migrations");
+
+    return {
+        service: service,
+        ui: ui,
+        migrations: migrations
+    }
+};
+
+const constructRepoAndBuildImage = async (imageTag: string, name: string, repoName: string) => {
+    const repo = new aws.ecr.Repository(`${name}-repo`, {
+        name: repoName,
+        imageTagMutability: "MUTABLE"
+    });
+
+    const repoCreds = await getEcrCredentials(repo);
+    const taggedImageName = pulumi.interpolate `${repo.repositoryUrl}:${imageTag}`;
+    const remoteImage = new docker.RemoteImage(`${name}-remote`, {
+        name: `${repoName}:${imageTag}`
+    });
+
+    const tag = new local.Command(`${name}-pull-tag`, {
+        create: pulumi.interpolate `docker tag ${remoteImage.name} ${taggedImageName}`
+    });
+
+    const image = new materialDocker.Image(`${name}-image`, {
+        name: taggedImageName,
+        registry: repoCreds,
+        context: "./dummy_docker",
+        args: [{
+            name: "SOURCE_IMAGE",
+            value: taggedImageName
+        }]
+    }, { dependsOn: [tag] });
+
+    return {
+        repo: repo.repositoryUrl,
+        image: image.name
+    }
+}
+
+const getEcrCredentials = async (repo: aws.ecr.Repository) => {
     const creds = repo.registryId.apply(async id => {
         const credentials = await aws.ecr.getCredentials({ registryId: id });
         const decodedCredentials = Buffer.from(credentials.authorizationToken, "base64").toString();
@@ -21,42 +69,36 @@ const getEcrCredentials = (repo: aws.ecr.Repository): docker.ImageRegistry => {
     return creds;
 };
 
-const config = new pulumi.Config();
-const imageTag = config.require("imageTag");
 
-const pulumiServiceRepo = new aws.ecr.Repository("pulumi-service-repo", {
-    name: "pulumi/service",
-    imageTagMutability: "MUTABLE"
-});
 
-const serviceImage = new docker.RemoteImage("service-image", {
-   name: `pulumi/service:${imageTag}` 
-});
+// const serviceImage = new docker.RemoteImage("service-image", {
+//     name: `pulumi/service:${imageTag}`
+// });
 
-const pulumiServiceCreds = getEcrCredentials(pulumiServiceRepo);
+// const pulumiServiceCreds = getEcrCredentials(pulumiServiceRepo);
 
-new docker.Image("push-service", {
-    imageName: pulumi.interpolate `${pulumiServiceRepo.repositoryUrl}:${imageTag}`,
-    registry: pulumiServiceCreds,
-    build: {
-        context: "./dummy_docker",
-        args: {
-            "SOURCE_IMAGE": serviceImage.name
-        }
-    }
-})
+// new docker.Image("push-service", {
+//     imageName: pulumi.interpolate`${pulumiServiceRepo.repositoryUrl}:${imageTag}`,
+//     registry: pulumiServiceCreds,
+//     build: {
+//         context: "./dummy_docker",
+//         args: {
+//             "SOURCE_IMAGE": serviceImage.name
+//         }
+//     }
+// })
 
-const pulumiConsoleRepo = new aws.ecr.Repository("pulumi-console-repo", {
-    name: "pulumi/console",
-    imageTagMutability: "MUTABLE"
-});
+// const pulumiConsoleRepo = new aws.ecr.Repository("pulumi-console-repo", {
+//     name: "pulumi/console",
+//     imageTagMutability: "MUTABLE"
+// });
 
-// const pulumiConsoleCreds = getEcrCredentials(pulumiConsoleRepo);
+// // const pulumiConsoleCreds = getEcrCredentials(pulumiConsoleRepo);
 
-const pulumiMigrationsRepo = new aws.ecr.Repository("pulumi-migrations", {
-    name: "pulumi/migrations",
-    imageTagMutability: "MUTABLE"
-});
+// const pulumiMigrationsRepo = new aws.ecr.Repository("pulumi-migrations", {
+//     name: "pulumi/migrations",
+//     imageTagMutability: "MUTABLE"
+// });
 
 // const pulumiMigrationsCreds = getEcrCredentials(pulumiMigrationsRepo);
 
@@ -74,6 +116,6 @@ const pulumiMigrationsRepo = new aws.ecr.Repository("pulumi-migrations", {
 //     name: `pulumi/migrations:${imageTag}`
 // });
 
-export const serviceRepoName = pulumiServiceRepo.name;
-export const consoleRepoName = pulumiConsoleRepo.name;
-export const migrationsRepoName = pulumiMigrationsRepo.name;
+// export const serviceRepoName = pulumiServiceRepo.name;
+// export const consoleRepoName = pulumiConsoleRepo.name;
+// export const migrationsRepoName = pulumiMigrationsRepo.name;
